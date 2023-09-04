aacc128fd2f797587c7edb4f64cc32643fd8cddc
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-09-04 11:00:09 -0400

    Add more routes to handle single product page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
 
-import { Navbar, Homepage, AllProducts } from './components/index';
+import { Navbar, Homepage, AllProducts, SingleProduct } from './components/index';
 import './App.scss';
 
 import GeneralContext from './contexts/GeneralContext';
@@ -11,6 +11,10 @@ function App() {
   const [products, setProducts] = useState([]);
   const [carousel, setCarousel] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [specifications, setSpecifications] = useState({
+    sizes: [],
+    colors: []
+  });
 
   useEffect(() => {
 
@@ -30,6 +34,13 @@ function App() {
         setCarousel(prev => res.data.images);
       });
 
+    axios.get('http://localhost:5001/api/specifications')
+      .then((res) => {
+        const sizes = res.data.sizes;
+        const colors = res.data.colors;
+        setSpecifications({ sizes, colors });
+      });
+
   }, []);
 
   useEffect(() => {
@@ -39,11 +50,13 @@ function App() {
 
   return (
     <>
-      <GeneralContext.Provider value={{ products, carousel, selectedCategory, setSelectedCategory }} >
+      <GeneralContext.Provider value={{ products, carousel, selectedCategory, setSelectedCategory, specifications }} >
         <Navbar />
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/products/:id' element={<AllProducts />} />
+          <Route path='/products/men/:id' element={<SingleProduct />} />
+          <Route path='/products/women/:id' element={<SingleProduct />} />
         </Routes>
 
       </GeneralContext.Provider>
