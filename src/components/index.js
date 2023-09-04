d2bed2603f88aca487b8d469a4e1f56128856c99
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-09-04 11:00:40 -0400

    Export single product component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Homepage } from "./Homepage/Homepage";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as AllProducts } from "./Products/AllProducts";
+export { default as SingleProduct } from "./SingleProduct/SingleProduct"
 
