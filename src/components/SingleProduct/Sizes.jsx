a1305e5957ef49c696ee05c1046af6d18f4ba261
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-09-06 14:03:43 -0400

    Create code to handle size buttons and size state

--- src/components/SingleProduct/Sizes.jsx
+++ src/components/SingleProduct/Sizes.jsx
@@ -3,17 +3,28 @@ import GeneralContext from '../../contexts/GeneralContext';
 
 import './sizes.scss';
 
-const Sizes = () => {
+const Sizes = ({ onSelectSize, selectedSize, setSelectedSize }) => {
   const { specifications } = useContext(GeneralContext);
   const sizes = specifications.sizes;
+  console.log(selectedSize);
+
+  const sizesArray = sizes.map((size) => {
+
+    let className = selectedSize === size.size ? "size-button active-size" : "size-button";
+    return (
+      <button
+        key={size.size}
+        className={className}
+        onClick={() => setSelectedSize(size.size)}
+      >
+        {size.size}
+      </button>
+    );
+  });
 
   return (
     <div className='all-sizes'>
-      {sizes.map((size, index) => (
-        <div key={index}>
-          <button className='size-button' >{size.size}</button>
-        </div>
-      ))}
+      {sizesArray}
     </div>
   );
 };
