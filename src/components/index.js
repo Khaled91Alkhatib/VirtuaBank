c8b69744835c83399de041a0b5c2599f87739282
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-09-12 09:18:46 -0400

    Export cart

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { default as Homepage } from "./Homepage/Homepage";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as AllProducts } from "./Products/AllProducts";
 export { default as SingleProduct } from "./SingleProduct/SingleProduct"
+export { default as Cart } from "./Cart/Cart"
 
